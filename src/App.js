94f176ed0f1462555e572bc278e917e7d86f33c1
ichchha-hub <ichchhadeshmukh35@gmail.com>
2020-07-12 22:35:54 +0200

    Added loading component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
-import { Counter } from "./features/counter/Counter";
+import Loading from "./components/Loading";
+import CssBaseline from "@material-ui/core/CssBaseline";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <CssBaseline />
+      <Loading />
     </div>
   );
 }
