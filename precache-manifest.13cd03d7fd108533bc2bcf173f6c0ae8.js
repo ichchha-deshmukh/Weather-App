c8dea119f8310ceea860a60029cd274cd4c2008e
ichchha-deshmukh <ichchhadeshmukh35@gmail.com>
2020-07-19 12:13:21 +0200

    Updates

---
+++ precache-manifest.13cd03d7fd108533bc2bcf173f6c0ae8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "552f3d1e40e0ec2c964b73b8161f17fb",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "8856c2eff51b27f75346",
+    "url": "/weather-app/static/css/main.fb22e3fb.chunk.css"
+  },
+  {
+    "revision": "561e3ff62aee026e50d6",
+    "url": "/weather-app/static/js/2.5c98a730.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/weather-app/static/js/2.5c98a730.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8856c2eff51b27f75346",
+    "url": "/weather-app/static/js/main.35f560c3.chunk.js"
+  },
+  {
+    "revision": "d6ddb8398bb990834374",
+    "url": "/weather-app/static/js/runtime-main.c24ff2e3.js"
+  },
+  {
+    "revision": "3e0450991cabbd44d7208277610d1569",
+    "url": "/weather-app/static/media/Arrow.3e045099.png"
+  }
+]);
